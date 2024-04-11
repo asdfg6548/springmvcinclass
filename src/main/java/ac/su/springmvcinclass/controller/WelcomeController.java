241ae9f9a51e2e263737d9c751d6ddb2e752ba34
asdfg6548 <asdfg6548@syuin.ac.kr>
2024-04-11 17:18:35 +0900

    Controller에서 요청 데이터 (param, body) 참조하기

--- src/main/java/ac/su/springmvcinclass/controller/WelcomeController.java
+++ src/main/java/ac/su/springmvcinclass/controller/WelcomeController.java
@@ -1,2 +1,15 @@
-package ac.su.springmvcinclass.controller;public class WelcomeController {
+package ac.su.springmvcinclass.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/example")
+public class WelcomeController {
+    @GetMapping("/")
+    public String Welcome() {
+        // GET 요청 처리 로직
+        return "index";
+    }
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,10 +1,11 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
 <%@ taglib prefix="c" uri="jakarta.tags.core" %>
 <html>
-  <head>
-    <title>Title</title>
-  </head>
-  <body>
-  
-  </body>
+<head>
+    <title>Index</title>
+</head>
+<body>
+<h1>사이트 홈페이지 입니다.</h1>
+<p>환영합니다.</p>
+</body>
 </html>
